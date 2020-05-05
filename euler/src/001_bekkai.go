b48a3b1383aa4b8b37a8a70e3a72fefcba49153b
kdaichi <sawaranokirimi@gmail.com>
2020-05-05 18:45:26 +0900

    fix difinition order

--- euler/src/001_bekkai.go
+++ euler/src/001_bekkai.go
@@ -2,20 +2,22 @@ package main
 
 import "fmt"
 
+func main() {
+	const MAX int = 1000
+
+	// 1000 は含まないので、1000 - 1 してから計算
+	answer := Sum(Filter(List(MAX - 1)))
+
+	fmt.Printf("Ans. %v\n", answer)
+	fmt.Println("done")
+}
+
 // 配列の各要素の値を足し合わせる
 func Sum(xs []int) int {
 	if len(xs) == 1 {
 		return xs[0]
 	}
 	return xs[0] + Sum(xs[1:])
-}
-
-// 例. 10 なら [1, 2, 3, ... 10] を返す
-func List(n int) []int {
-	if n == 1 {
-		return []int{1}
-	}
-	return append(List(n-1), n)
 }
 
 // 3 or 5 で割り切れない要素を配列から除外
@@ -31,9 +33,10 @@ func Filter(xs []int) []int {
 	return Filter(xs[1:])
 }
 
-func main() {
-	const MAX int = 1000
-
-	// 1000 は含まないので、1000 - 1 する
-	fmt.Printf("Ans. %v", Sum(Filter(List(MAX-1))))
+// 10 なら [1, 2, 3, ... 10] を返す
+func List(n int) []int {
+	if n == 1 {
+		return []int{1}
+	}
+	return append(List(n-1), n)
 }
